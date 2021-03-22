22d3fde6591b63e63a91533ec8788bff628ae87d
callumparker64 <callumparker64@gmail.com>
2021-03-22 20:41:39 +0000

    Constructor

--- Steal.cs
+++ Steal.cs
@@ -14,6 +14,34 @@ namespace SpatialGames
             Score = score;
         }
 
+        // public void Adjacency()
+        // {
+
+        //     for (int i = 0; i < 2; i++)
+        //     {
+        //         for (int j = 0; j < 2; j++)
+        //         {
+        //             if(i == 2 && j == 2)
+        //             {
+        //                 break;
+        //             }
+        //             else if(//adj is Split)
+        //             {
+        //                 Score = Score + 1;
+        //             }
+        //             else if(//adj is Steal)
+        //             {
+        //                 Score = Score - 1;
+        //             }
+        //             else if(//adj is TitForTat)
+        //             {
+        //                 //
+        //             }
+                    
+        //         }
+        //     }
+        // }
+
 
 
     }
